493e846217f19e8f00fad90ddf52f21243d7d602
Scherbakova <nastyasch0806@gmail.com>
2022-08-21 23:45:33 +0300

    hw-stroki

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,33 @@
 import javax.naming.InvalidNameException;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main (String[] args)
+    {
+        task1();
+        task2();
+        task3();
+    }
+    public static void task1() {
 
-
-        String phone = "+960-415 75-37";
-        phone = phone.replace("-", "");
-        phone = phone.replace(" ", "");
-        phone = phone.replace("+", "");
-        if (phone.length() == 10) {
-            phone = '7' + phone;
-        } else if (phone.length() > 11) {
-            throw new RuntimeException("телефон слишком длинный");
-        } else if (phone.length() < 10) {
-            throw new RuntimeException("телефон слишком короткий");
-        } else if (phone.length() == 11 && phone.charAt(0) != '7') {
-            throw new RuntimeException("Среди нас посторонний");
-        }
-        System.out.println("phone = " + phone);
-
-        String expectedPhone = "79604157537";
-        if (phone.equals(expectedPhone)) {
-            System.out.println("Успех");
-        } else {
-            System.out.println("Неудача");
-        }
-
-
-        //task1
         String firstName = " Ivan ";
         String middleName = " Ivanovich";
         String lastName = "Ivanov ";
         String fullName = lastName + firstName + middleName;
         System.out.println("ФИО сотрудника - " + fullName);
+    }
+    public static void task2()
+    {
 
+        String fullName = "Ivanov Ivan Ivanovich";
+        String fullname2 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullname2);
+    }
 
-        //task2
-        String fullName2 = fullName.toUpperCase();
-        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName2);
+    public static void task3(){
 
-
-        //task3
-
-        String fullName3 = "Иванов Семён Семёнович";
-        fullName3 = fullName3.replace("ё", "e");
-        System.out.println("Данные ФИО сотрудника — " + fullName3);
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "e");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
 
 
     }
